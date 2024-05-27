4973eb903a71d17836f27a90b1d00fe9f7d585dd
Nathan <nathan.tournant@gmail.com>
2024-05-27 13:33:50 +0200

    feat : Ajout de l'entrée utilisateur pour le pays et creation de la fonction PlusGrandVille pour clarifier le code

--- Program.cs
+++ Program.cs
@@ -7,26 +7,38 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var codepays = "FR";
-        var utilisateur = "natther";
-        var url = $"http://api.geonames.org/searchJSON?formatted=true&country={codepays}&maxRows=15&cities=cities15000&orderby=population&username={utilisateur}";
+        Console.WriteLine("Veuillez entrer un code de pays, (exemple FR) :");
+        string codePays = Console.ReadLine();
 
-        HttpClient client = new HttpClient();
-        var response = await client.GetAsync(url);
-        response.EnsureSuccessStatusCode();
-
-        var responseBody = await response.Content.ReadAsStringAsync();
-        var cities = JsonSerializer.Deserialize<GeoNamesResponse>(responseBody);
-
-        foreach (var city in cities.geonames)
+        if (!string.IsNullOrEmpty(codePays))
         {
-            Console.WriteLine($"{city.name}");
+            await PlusGrandVille(codePays);
+        }
+        else
+        {
+            Console.WriteLine("Entrée invalide. Veuillez entrer un code de pays valide.");
         }
     }
 
+    private static async Task PlusGrandVille(string codePays)
+    {
+        string utilisateur = "natther";
+        string url = $"http://api.geonames.org/searchJSON?formatted=true&country={codePays}&maxRows=15&cities=cities15000&orderby=population&username={utilisateur}";
+
+        using HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        GeoNamesResponse villes = JsonSerializer.Deserialize<GeoNamesResponse>(responseBody);
+
+        Console.WriteLine($"Les plus grandes villes en {codePays} :");
+        foreach (var ville in villes.geonames)
+        {
+            Console.WriteLine($" {ville.name} ");
+        }
+    }
 }
-
-
 
 public class GeoNamesResponse
 {
